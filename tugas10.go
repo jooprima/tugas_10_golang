0b9a66757eeaf80e122be7e5bf6f1b8ed9d29ed8
yohanes_prima <jooprima@gmail.com>
2019-10-19 22:48:46 +0700

    tugas 10 golang
    tugas ini dikumpulkan untuk NIOMIC

---
+++ tugas10.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	fmt.Println("Yohanes Prima")
+	time.Sleep(time.Second * 5)
+	fmt.Println("hobinya ngoding")
+	time.Sleep(time.Second * 5)
+	fmt.Println("Jakarta 5 april 1991")
+}
